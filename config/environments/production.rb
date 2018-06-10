c1854902112f413921782a4d36fc8afb2e7f7ff1
Kadumasa Sato <k-satou@digi-k.com>
2018-06-10 10:09:31 +0900

    Change default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'secure-mesa-73388.herokuapp.com'
+  host = 'www.celaeno.site'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
