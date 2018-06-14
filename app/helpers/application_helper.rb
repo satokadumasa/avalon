a258dd0a9a82833fd4ffb563bd4c47da7bb124d1
Kadumasa Sato <k-satou@digi-k.com>
2018-06-14 23:33:05 +0900

    タグ変換ヘルパーメソッド実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def tag_convert(text)
+    tags = [
+      'strong',
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+    ]
+    ws = ['(\W+)','(\w+)']
+    tags.each do |tag|
+      ws.each do |w|
+        t = "<#{tag}:#{w}>"
+        mds = text.scan(/#{t}/)
+        if mds = text.scan(/#{t}/)
+          mds.each do |md|
+            if md[0]
+              text = text.gsub(/<#{tag}:#{md[0]}>/, "<#{tag}>#{md[0]}</#{tag}>") 
+            end
+          end
+        end
+      end
+    end
+    text
+  end
 end
--- config/initializers/00_load_config.rb
+++ config/initializers/00_load_config.rb
@@ -1,2 +1,3 @@
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")[Rails.env]
+TAG_SETTING = YAML.load_file("#{Rails.root}/config/tags.yml")
 SOME_CONSTANT = ""
