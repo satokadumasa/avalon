8ace52490f1d9b073e1d444945024b19cdb9de60
Kadumasa Sato <k-satou@digi-k.com>
2018-07-31 12:41:29 +0900

    Tag.add_count 引数エラー修正（３）

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,16 @@
 class Tag < ApplicationRecord
   def self.add_count(tag_names, tags)
-    logger.debug "Tag.add_count tag_names:" + tag_names.inspect
+    logger.info "Tag.add_count tag_names:" + tag_names.inspect
+    logger.info "Tag.add_count tags:" + tags.inspect
     tag_names.each do |tag_name|
       next if tags.include?(tag_name)
       tag = Tag.where(name: tag_name).first
       if tag
+        logger.info "Tag.add_count update"
         tag[:cnt] = (tag[:cnt] == 0 || !tag[:cnt]) ? 1 : tag[:cnt] + 1
         tag.save
       else
+        logger.info "Tag.add_count add tag."
         tag = Tag.new({name: tag_name, cnt: 1})
         tag.save
       end
