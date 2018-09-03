139a08d0e7c82df5a389ae7a1cada184909f7f81
Kadumasa Sato <k-satou@digi-k.com>
2018-09-03 19:11:37 +0900

    メール送信設定変更(3)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
-  # host = 'www.selaeno.com'
-  # config.action_mailer.default_url_options = { host: host }
+  host = 'www.selaeno.com'
+  config.action_mailer.default_url_options = { host: host }
   # ActionMailer::Base.smtp_settings = {
   #   :address        => 'smtp.sendgrid.net',
   #   :port           => '587',
@@ -109,5 +109,8 @@ Rails.application.configure do
   #   :domain         => 'heroku.com',
   #   :enable_starttls_auto => true
   # }
+  host = 'www.selaeno.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :ses
+  config.mailer_sender = 'WebMaster <webmaster@selaeno.com>'
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -7,5 +7,5 @@ S3_BUCKET = Aws::S3::Resource.new.bucket('celaenoimages')
 ActionMailer::Base.add_delivery_method :ses,
                                        AWS::SES::Base,
                                        access_key_id: ENV['AWS_ACCESS_KEY_ID2'],
-                                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY2'],
-                                       server: 'feedback-smtp.us-east-1.amazonses.com'
+                                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY2']#,
+                                       server: 'email-smtp.us-east-1.amazonaws.com'
